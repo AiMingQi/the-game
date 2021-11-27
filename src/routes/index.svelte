c00429a99173fdbf81c3e8e7fd34d6510137b770
AiMingQi <cafevalentine@gmail.com>
2021-11-27 11:44:32 -0700

    add basic site

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Russo+One&display=swap");
+    </style>
+</svelte:head>
+<div class="headbox"> 
+<h1 >Welcome to the-game</h1>
+<p></p>
+</div>
+
+<style>
+     h1 {
+        padding: 20 0;
+        text-align: center;
+        color: #c00000;
+        font-family: 'Russo One', sans-serif;;
+    }
+    .headbox {
+        margin: 100px 0;
+    }
+</style>
