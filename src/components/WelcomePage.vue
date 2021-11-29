e8a67ad61439b3499eea09a7bb44244c5b40df67
AiMingQi <cafevalentine@gmail.com>
2021-11-29 14:13:11 -0700

    update structure and add store value

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -26,8 +26,8 @@
           <h1 class="mb-3" >♥ + - ?</h1>
         </v-row>
         <v-row justify="center">
-          <v-btn @click="getOwnerAddress" v-show="!ownerAddress">Login</v-btn>
-          <p v-show="ownerAddress">Welcome <br> <strong>{{ownerAddress}}</strong></p>
+          <v-btn @click="getOwnerAddress" v-show="$store.state.ownerAddress == 'nothing'">Login</v-btn>
+          <p v-show="$store.state.ownerAddress">Welcome <br> <strong>{{$store.state.ownerAddress}}</strong></p>
         </v-row>
       </v-col>
 
@@ -83,16 +83,17 @@
           text: 'Get an NFT to play',
           href: 'https://solsea.io/collection/616eee186c530ec596bb1027',
         },
-      ],
+      ]
     }),
     mounted () {
       console.log(solanaWeb3);
+      console.log(this.$store.state.ownerAddress);
     },
     methods: {
       async getOwnerAddress(){
         try {
           const resp = await window.solana.connect();
-          this.ownerAddress = resp.publicKey.toString()
+          this.$store.state.ownerAddress = resp.publicKey.toString()
           console.log(resp.publicKey.toString())
           // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo 
         } catch (err) {
